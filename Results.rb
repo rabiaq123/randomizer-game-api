b690929442d2bb92f330ce652bf45d701cad48bc
Rabia Qureshi <rabia.q123@gmail.com>
2022-09-24 22:10:54 -0400

    Rename local variables in Results class methods for clarity

--- Results.rb
+++ Results.rb
@@ -7,50 +7,50 @@ class Results
         @description = nil
     end
 
-    def description(description) # Stores a description from which the Randomizer objects stored in self can be selected when computing the results, tally or sum 
+    def description(description) # stores a description from which the Randomizer objects stored in self can be selected when computing the results, tally or sum 
         @description = description
         return nil
     end
 
     def results() # returns array containing the “side-up” values of the randomizers recorded in self
-        # Only include the values from randomizers that match the description stored in the Results.
-        # If description() has not yet been called, return the results from all randomizers
-        results = []
+        # only include the values from randomizers that match the description stored in the Results.
+        # if description() has not yet been called, return the results from all randomizers
+        sideup_values = []
         @cup.randomizers.each do |randomizer|
-            if @description == nil || randomizer.description == @description
-                results << randomizer.sideup()
+            if @description == nil || @description == randomizer.description
+                sideup_values << randomizer.sideup()
             end
         end
-        return results
+        return sideup_values
     end
 
     def tally() # returns the number of items in the Results that match the description 
         # if description() has not yet been called, count all randomizers
-        tally = 0
+        num_matches = 0
         @cup.randomizers.each do |randomizer|
-            if @description == nil || randomizer.description == @description
-                tally += 1
+            if @description == nil || @description == randomizer.description
+                num_matches += 1
             end
         end
-        return tally
+        return num_matches
     end
 
     def sum()
-        # totals the value of the randomizer items in the Results that match the description, where the value equals the number that is “up” (for coins, :H = 1 and :T = 0), and returns the value
+        # totals the value of the Randomizer items in the Results that match the description, where the value equals the number that is “up” (for coins, :H = 1 and :T = 0), and returns the value
         # if description() has not yet been called, total the values across all randomizers
-        sum = 0
+        sideup_sum = 0
         @cup.randomizers.each do |randomizer|
             if @description == nil || randomizer.description == @description
-                if randomizer.sideup() == :H
-                    sum += 1
-                elsif randomizer.sideup() == :T
-                    sum += 2
+                if randomizer.sideup() == :H # did not check for :T because it will not affect sideup_sum
+                    sideup_sum += 1
+                # elsif randomizer.sideup() == :T
+                #     sideup_sum += 2
                 elsif randomizer.sideup().is_a? Integer # for dice
-                    sum += randomizer.sideup()
+                    sideup_sum += randomizer.sideup()
                 end
             end
         end
-        return sum
+        return sideup_sum
     end
 
 end

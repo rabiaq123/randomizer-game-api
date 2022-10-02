0287112786920a85ce80ff5132750d9a56d9a072
Rabia Qureshi <rabia.q123@gmail.com>
2022-10-02 15:45:14 -0400

    Added test for 'Advancing in spots on game board' use case

--- testgames.rb
+++ testgames.rb
@@ -21,9 +21,9 @@ class TestUseCases < Test::Unit::TestCase
         coin1 = Coin.new(0.25)
         coin2 = Coin.new(0.25)
         coin3 = Coin.new(0.25)
-        assert(coin1.sideup == nil && coin1.denomination == 0.25, "Coin 1's sideup value should not be set before being flipped, and should have a denomination of 0.25")
-        assert(coin2.sideup == nil && coin2.denomination == 0.25, "Coin 2's sideup value should not be set before being flipped, and should have a denomination of 0.25")
-        assert(coin3.sideup == nil && coin3.denomination == 0.25, "Coin 3's sideup value should not be set before being flipped, and should have a denomination of 0.25")
+        assert({:sides=>2, :up=>nil, :item=>:coin, :denomination=>0.25} == coin1.description, "Coin 1's sideup value should not be set before being flipped, and should have a denomination of 0.25")
+        assert({:sides=>2, :up=>nil, :item=>:coin, :denomination=>0.25} == coin2.description, "Coin 2's sideup value should not be set before being flipped, and should have a denomination of 0.25")
+        assert({:sides=>2, :up=>nil, :item=>:coin, :denomination=>0.25} == coin3.description, "Coin 3's sideup value should not be set before being flipped, and should have a denomination of 0.25")
 
         # put players' coins in thier bags
         puts "\tPutting each player's coin in their bag"
@@ -58,6 +58,9 @@ class TestUseCases < Test::Unit::TestCase
         assert_equal(coin1.calls, 3, "Coin 1 should have been flipped three times")
         assert_equal(coin2.calls, 3, "Coin 2 should have been flipped three times")
         assert_equal(coin3.calls, 3, "Coin 3 should have been flipped three times")
+        assert(coin1.sideup == :H || coin1.sideup == :T, "Coin 1 should have a sideup value of :H or :T")
+        assert(coin2.sideup == :H || coin2.sideup == :T, "Coin 2 should have a sideup value of :H or :T")
+        assert(coin3.sideup == :H || coin3.sideup == :T, "Coin 3 should have a sideup value of :H or :T")
         
         # get results of all coin flips
         puts "\tGetting results of all coin flips"
@@ -66,6 +69,79 @@ class TestUseCases < Test::Unit::TestCase
         puts "\tplayer1.tally: #{player1.tally(desc).tally()}"
         puts "\tplayer2.tally: #{player2.tally(desc).tally()}"
         puts "\tplayer3.tally: #{player3.tally(desc).tally()}"
+    end
+    
+    def test_uc_3 
+        puts "\n\nTesting Use Case 3: Advancing in Spots on a Game Board\n"
+
+        # create all players
+        puts "\tCreating 'Player 1', 'Player 2', and 'Player 3'"
+        player1 = Player.new("Player 1")
+        player2 = Player.new("Player 2")
+        assert_equal(player1.name, "Player 1", "Player 1 should have name Player 1")
+        assert_equal(player2.name, "Player 2", "Player 2 should have name Player 2")
+        # create player coins
+        puts "\tCreating two dice for each player"
+        die1 = Die.new(6, :yellow) # player 1
+        die2 = Die.new(6, :yellow)
+        die3 = Die.new(6, :blue) # player 2
+        die4 = Die.new(6, :blue)
+        assert({:sides=>6, :up=>nil, :item=>:die, :colour=>:yellow} == die1.description, "Die is missing necessary attributes")
+        assert_equal(die1.description, die2.description, "Die 1 and Die 2 should have the same description")
+        assert({:sides=>6, :up=>nil, :item=>:die, :colour=>:blue} == die3.description, "Die is missing necessary attributes")
+        assert_equal(die3.description, die4.description, "Die 3 and Die 4 should have the same description")
+
+        # put players' dice in thier hands
+        puts "\tPutting each player's dice in their hand"
+        p1_hand = Hand.new
+        p1_hand.store_all([die1, die2])
+        p2_hand = Hand.new
+        p2_hand.store_all([die3, die4])
+        assert_not_nil(p1_hand.randomizers, "Player 1's hand should have 2 dice")
+        assert_not_nil(p2_hand.randomizers, "Player 2's hand should have 2 dice")
+
+        # put players' coins in thier bags
+        puts "\tPutting each player's dice in their bag"
+        player1.move_all(p1_hand)
+        player2.move_all(p2_hand)
+        p1_bag = player1.bag
+        p2_bag = player2.bag
+        # ensure randomizers are in bag
+        assert_equal(p1_bag.randomizers.count, 2, "Player 1's bag should have 2 dice")
+        assert_equal(p2_bag.randomizers.count, 2, "Player 2's bag should have 2 dice")
+        assert(p1_bag.randomizers[0].sideup == nil && p1_bag.randomizers[1].sideup == nil, "Dice should not have a sideup value in bag")
+        assert(p2_bag.randomizers[0].sideup == nil && p2_bag.randomizers[1].sideup == nil, "Dice should not have a sideup value in bag")
+        # ensure randomizers are no longer in hand
+        assert_equal(p1_hand.randomizers, [], "Player 1's hand should be empty")
+        assert_equal(p2_hand.randomizers, [], "Player 2's hand should be empty")
+
+        # transfer coins from players' bags to the cup
+        puts "\tTransferring each player's dice from their bag to thier cup"
+        player1.load
+        assert_equal(p1_bag.randomizers, [], "Player 1's bag should be empty")
+        assert_not_equal(player1.cup.randomizers, [], "Player 1's cup should not be empty")
+        player2.load
+        assert_equal(p2_bag.randomizers, [], "Player 2's bag should be empty")
+        assert_not_equal(player2.cup.randomizers, [], "Player 2's cup should not be empty")
+
+        # roll dice in cup
+        puts "\tRolling dice"
+        player1.throw
+        player2.throw
+        # expect dice to be rolled once
+        assert_equal(die1.calls, 1, "Die 1 should have been flipped once")
+        assert_equal(die2.calls, 1, "Die 2 should have been flipped once")
+        assert_equal(die3.calls, 1, "Die 3 should have been flipped once")
+        assert_equal(die4.calls, 1, "Die 4 should have been flipped once")
+        assert(die1.sideup >= 1 && die1.sideup <= 6, "Die 1 should have a sideup value between 1 and 6")
+        assert(die2.sideup >= 1 && die2.sideup <= 6, "Die 2 should have a sideup value between 1 and 6")
+        assert(die3.sideup >= 1 && die3.sideup <= 6, "Die 3 should have a sideup value between 1 and 6")
+        assert(die4.sideup >= 1 && die4.sideup <= 6, "Die 4 should have a sideup value between 1 and 6")
+        
+        # get results of all player rolls
+        puts "\tGetting Player 1 and Player 2's dice results"
+        puts "\tplayer1: #{die1.sideup} + #{die2.sideup} = #{die1.sideup + die2.sideup}"
+        puts "\tplayer2: #{die3.sideup} + #{die4.sideup} = #{die3.sideup + die4.sideup}"
     end
 
 end
